715a37e03717e6bc3c4d0b79992ce1ae099080ac
James Walter <jameswalter695@gmail.com>
2026-04-06 13:14:58 +0100

    feat: ajout redirection WhatsApp avec message de don + amélioration UI navbar et slider

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://colemgirvanfrfzcrmpj.supabase.co';
-const supabaseKey = 'sb_publishable_HidATxKQ7WxKlf2wzm4Ikw_WpcsbR6F'; 
+const supabaseUrl = 'https://uxkvdjwiqoqelcyhyhdk.supabase.co';
+const supabaseKey = 'sb_publishable_pCUU4xX_-db_VyMTO115hQ_B6-MyhU_'; 
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
